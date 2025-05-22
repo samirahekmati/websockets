1f7dbaff130a55b4cda3dcd7359df3c2655f7300
Samira <samirahekmati@gmail.com>
2025-05-22 21:17:35 +0100

    client-Create WebSocket connection

--- frontend/script.js
+++ frontend/script.js
@@ -6,6 +6,20 @@ const form = document.getElementById("form");
 openWsButton.addEventListener('click', (e) =>{
     console.log("open ws button is clicked.")
     form.style.display = "block"
-    const ws = new WebSocket("ws://127.0.0.1:3000")
+   // Create WebSocket connection.
+//    The browser sends an HTTP upgrade request to the server saying:
+// “I’d like to upgrade this connection to WebSocket.”
+const socket = new WebSocket("ws://127.0.0.1:8080");
+
+// Connection opened
+socket.addEventListener("open", (event) => { //Registers a callback function to run once the WebSocket connection is successfully established.
+  socket.send("Hello Server!");//Sends the message "Hello Server!" to the server immediately after the connection is open.
+});
+
+// Listen for messages
+socket.addEventListener("message", (event) => {
+  console.log("Message from server ", event.data); //message: connection established
+});
     
 })
+
